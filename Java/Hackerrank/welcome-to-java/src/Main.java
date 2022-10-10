9c5e53fd497b1b718bfb3d74cdb2bdafe1237dfe
gulnisaslan <gulnisaslan@gmail.com>
2022-10-10 22:02:01 +0300

    First commit-Hackerrank algorithm question solutions 1

---
+++ Java/Hackerrank/welcome-to-java/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+	}
+
+}
