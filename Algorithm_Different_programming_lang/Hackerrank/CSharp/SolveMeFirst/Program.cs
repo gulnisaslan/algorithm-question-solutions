7721e40450e8fcb34a355f566f4073d5897ccf57
gulnisaslan <gulnisaslan@gmail.com>
2022-10-18 23:39:52 +0300

    added algorithm question answers-1

---
+++ Algorithm_Different_programming_lang/Hackerrank/CSharp/SolveMeFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SolveMeFirst
+{
+    class Program
+    {static int solveMeFirst(int a, int b) { 
+      int sumAandB = a+b;
+      return sumAandB;
+    }
+
+    static void Main(String[] args) {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = solveMeFirst(val1,val2);
+        Console.WriteLine(sum);
+    }
+    }
+}
