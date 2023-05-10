385fa6c3a2e3b134f80b05af93fa5f7bc3e73bb3
gulnisaslan <gulnisaslan@gmail.com>
2023-05-11 00:14:09 +0300

    I have started 30 of days of code in hackerrank

---
+++ hackerrank/30OfDaysCode/CsharpAlgortimQuestions/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public  static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+    }
+
+}
